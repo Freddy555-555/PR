fcc3666beaf95a91c2a3115feadafa991de190e1
Freddy555-555 <azerbaijanfootballarena@gmail.com>
2020-07-18 19:04:09 +0400

    Выполнил

--- src/WorkShop.java
+++ src/WorkShop.java
@@ -5,6 +5,14 @@ public class WorkShop {
     public static void main(String[] args){
 
 
+        boolean isBlock = true;
+        byte aByte = 3;
+        short aShort = 4;
+        int aInt = 23;
+        long aLong = 345324374534L;
+        double aDouble = 3.483248324d;
+        float aFloat = 4.323232f;
+        char aChar = 's';
 
 
 
